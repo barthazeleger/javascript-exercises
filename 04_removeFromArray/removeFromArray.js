126cd272ffa9381139eb377b571ea2eb60222900
Bart <bart@hazeleger.me>
2022-04-12 11:12:40 +0200

    fixed exercise 4
    Fixed this.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,7 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (createArray, pushFromArray) {
+  const myArray = createArray;
+  myArray.splice(pushFromArray - 1, 1);
+  return myArray;
 };
 
 // Do not edit below this line
