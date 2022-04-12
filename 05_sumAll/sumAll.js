8846390b23e03874081c009357d6b630b5a61470
Bart <bart@hazeleger.me>
2022-04-12 11:28:24 +0200

    Update sumAll.js
    Dk if this is the right way, but it works, and it feels awesome.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,15 @@
-const sumAll = function() {
-
+const sumAll = function (minNum, maxNum) {
+  let sum = 1;
+  let i = minNum;
+  while (i < maxNum) {
+    i++;
+    if (i === maxNum) {
+      sum += i;
+    } else {
+      sum += i;
+    }
+  }
+  return sum;
 };
 
 // Do not edit below this line
