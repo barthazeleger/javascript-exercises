8d3910800409008338d353211f2702646ed7591b
Bart <bart@hazeleger.me>
2022-04-12 10:49:53 +0200

    Update repeatString.js
    This was a puzzle. But I had to declare an empty string variable instead of just a variable, then in the for loop repeat the addition of the string to the empty variable and return the variable outside of the loop.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function (string, num) {
+  let repString = '';
+  for (let i = 0; i < num; i++) {
+    repString += string;
+  }
+  return repString;
 };
 
 // Do not edit below this line
